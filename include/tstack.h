7945514e2709dc04cb3ba5ff2993aae018372a7e
smirnovaalin <160322763+smirnovaalin@users.noreply.github.com>
2024-04-14 17:41:29 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -5,34 +5,33 @@
 template<typename T, int size>
 class TStack {
 private:
-    T data[100];
-    int top;
+T data[100];
+int top;
 
 public:
-    TStack() : top(-1) {}
-    void push(T value) {
-        if (!isFull())
-            data[++top] = value;
-        else
-            throw std::string("Full");
-    }
-    T get() const {
-        return data[top];
-    }
-    T pop() {
-        if (isEmpty()) {
-            throw std::string("Empty");
-        }
-        else {
-            return data[top--];
-        }
-    }
-    bool isEmpty() const {
-        return top == -1;
-    }
-    bool isFull() const {
-        return top == size - 1;
-    }
+TStack() : top(-1) {}
+void push(T value) {
+  if (!isFull())
+    data[++top] = value;
+  else
+  throw std::string("Full");
+}
+T get() const {
+  return data[top];
+}
+T pop() {
+  if (isEmpty()) {
+    throw std::string("Empty");
+  } else {
+    return data[top--];
+  }
+}
+bool isEmpty() const {
+  return top == -1;
+}
+bool isFull() const {
+  return top == size - 1;
+}
 };
 
 #endif  // INCLUDE_TSTACK_H_
